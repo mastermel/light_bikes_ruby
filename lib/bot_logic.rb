1ceb06fb70a0800a1a4fb6fcc6c04a2fa7ad3717
Davis Hyer <dhyer@instructure.com>
2018-10-12 11:25:17 -0600

    reset bot logic to basic

--- lib/bot_logic.rb
+++ lib/bot_logic.rb
@@ -1,5 +1,3 @@
-require "pry"
-
 class BotLogic
   attr_reader :client, :debug
 
@@ -10,8 +8,7 @@ class BotLogic
     test_game: false,
     name: 'Flynn',
     log_prefix: nil,
-    debug: false,
-    difficulty: nil
+    debug: false
   )
     @debug = debug
     @client = LightBikesClient.new(
@@ -21,7 +18,6 @@ class BotLogic
       log_prefix: log_prefix,
       player_count: player_count,
       server_uri: server_uri,
-      difficulty: difficulty,
     )
   end
 
@@ -67,51 +63,9 @@ class BotLogic
   end
 
   def find_best_move
-    player = client.player
+    # YOLO!
+    move = available_moves.sample
 
-    right_score = 1
-    loop do
-      break unless client.all_points[Point.new(player.x + right_score, player.y)].nil?
-      right_score += 1
-    end
-
-    left_score = -1
-    loop do
-      break unless client.all_points[Point.new(player.x + left_score, player.y)].nil?
-      left_score -= 1
-    end
-
-    down_score = 1
-    loop do
-      break unless client.all_points[Point.new(player.x, player.y + down_score)].nil?
-      down_score += 1
-    end
-
-    up_score = -1
-    loop do
-      break unless client.all_points[Point.new(player.x, player.y + up_score)].nil?
-      up_score -= 1
-    end
-
-    scores = {
-      right: right_score,
-      left: left_score,
-      down: down_score,
-      up: up_score
-    }
-
-    direction = scores.max_by{|k,v| v.abs}[0]
-
-    move = case direction
-    when :right
-      Point.new(player.x + 1, player.y)
-    when :left
-      Point.new(player.x - 1, player.y)
-    when :down
-      Point.new(player.x, player.y + 1)
-    when :up
-      Point.new(player.x, player.y - 1)
-    end
     # If we haven't found someting yet I guess we're dead! Take the first move
     move || available_moves(all: true).first
   end
